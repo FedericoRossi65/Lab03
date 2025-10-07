27a5cf5a5c4819d36bae42a473a974b6e1d1ce20
Federico <frossimiglio@gmail.com>
2025-10-07 22:30:59 +0200

    Implementato funzioni di carica_file e ordina_automobili,aggiungi automobile e nuovo noleggio, creato la classe Auto e la classe Noleggio, ancora da completare la funzione termina noleggio

---
+++ Noleggio.py
@@ -0,0 +1,8 @@
+class Noleggio:
+    def __init__(self, id_noleggio, data,id_automobile,cognome_cliente):
+        self.id_noleggio = id_noleggio
+        self.data = data
+        self.id_automobile = id_automobile
+        self.cognome_cliente = cognome_cliente
+    def __str__(self):
+        return f'Dati noleggio: {self.id_noleggio} {self.data} {self.id_automobile} {self.cognome_cliente} '
--- autonoleggio.py
+++ autonoleggio.py
@@ -1,9 +1,16 @@
 import csv
 from Auto import Auto
+from Noleggio import Noleggio
+
+
 class Autonoleggio:
     def __init__(self, nome, responsabile):
         """Inizializza gli attributi e le strutture dati"""
         self.lista_auto = []
+        self.nome = nome
+        self.responsabile = responsabile
+        self.auto_noleggiate = []
+
 
 
 
@@ -29,7 +36,9 @@ class Autonoleggio:
 
     def aggiungi_automobile(self, marca, modello, anno, num_posti):
         """Aggiunge un'automobile nell'autonoleggio: aggiunge solo nel sistema e non aggiorna il file"""
-        # TODO
+        auto_nuova = marca,modello,anno,num_posti
+        self.lista_auto.append(auto_nuova)
+        return auto_nuova
 
     def automobili_ordinate_per_marca(self):
         """Ordina le automobili per marca in ordine alfabetico"""
@@ -42,9 +51,21 @@ class Autonoleggio:
 
     def nuovo_noleggio(self, data, id_automobile, cognome_cliente):
         """Crea un nuovo noleggio"""
-        # TODO
+        id_noleggio= f'N{len(self.auto_noleggiate)+1}'
+        nuovo_noleggio = Noleggio(id_noleggio,data,id_automobile,cognome_cliente)
+        self.auto_noleggiate.append(nuovo_noleggio)
+        return f'ID del noleggio effeuttuato: {id_noleggio}'
+
+
 
 
     def termina_noleggio(self, id_noleggio):
         """Termina un noleggio in atto"""
-        # TODO
+        for automobili in self.auto_noleggiate:
+            if automobili.id_noleggio == id_noleggio:
+                self.auto_noleggiate.remove(automobili)  # attenzione devo ancora mdificare questa funzione perchè nel main non mi da errore anche se il codice noleggio non esiste
+                return id_noleggio
+        return None
+
+
+
